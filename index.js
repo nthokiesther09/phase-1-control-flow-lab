89f3eb886fb014ac46c5d1ee55d02a13d1b3697b
nthokiesther09 <nthokiesther09@gmail.com>
2022-11-25 14:20:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+let fee = 0
+  if (ride <= 400){
+  return'This one is on me!';
+} else if (ride>2000 && ride <= 2500){
+  fee+=30;
+  return 'I will gladly take your thirty bucks.';
+} else if (ride>2500){
+  return 'No can do.';}
+}
+  
   // Write your code here!
+
+
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+  return 'Ok, sounds good.';
+} else if (city !== 'NYC'){
+ return 'No go.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      case "not as generous":
+        return 'Thank you.';
+        default:
+          return 'Bye.';
+  }
   // Write your code here!
 }
